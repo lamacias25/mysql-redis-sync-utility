0f7bba3a4b9804488ce50d8222fc5f9c07390014
lamacias25 <la.macias@mavi.mx>
2022-08-30 14:11:27 -0500

    Update mysql-redis-sync.py

--- mysql-redis-sync.py
+++ mysql-redis-sync.py
@@ -9,8 +9,8 @@ from pymysqlreplication.row_event import (
 MYSQL_SETTINGS = {
     "host": "127.0.0.1",
     "port": 3306,
-    "user": "root",
-    "passwd": "root"
+    "user": "mavi",
+    "passwd": "mavi"
 }
 
 ONLY_Tables = {
