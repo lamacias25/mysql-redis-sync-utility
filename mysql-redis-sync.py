79e256eed9d26cfcb1453363d0b81bd24e637cee
lamacias25 <la.macias@mavi.mx>
2022-08-30 14:03:39 -0500

    python3

--- mysql-redis-sync.py
+++ mysql-redis-sync.py
@@ -10,7 +10,7 @@ MYSQL_SETTINGS = {
     "host": "127.0.0.1",
     "port": 3306,
     "user": "root",
-    "passwd": "mysql"
+    "passwd": "root"
 }
 
 ONLY_Tables = {
@@ -36,14 +36,14 @@ def diff(first, second):
 def updateRediSCache(insertupdate,prefix,keys, row):
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     for key in keys:	    
-	cacheKey = prefix + key
-	if insertupdate: 	
-	    cacheValue = row[key]
-	    r.set(cacheKey,cacheValue);
-            print "Updated Cache -> " +  cacheKey  + " : " + cacheValue
-	else:
-	    r.delete(cacheKey)
-	    print "Updated Cache -> " +  cacheKey  + " Removed "	
+        cacheKey = prefix + key
+        if insertupdate: 	
+            cacheValue = row[key]
+            r.set(cacheKey,cacheValue);
+            print("Updated Cache -> " +  cacheKey  + " : " + cacheValue)
+        else:
+            r.delete(cacheKey)
+            print("Updated Cache -> " +  cacheKey  + " Removed ")	
 
 def main():
     stream = BinLogStreamReader(
@@ -51,21 +51,21 @@ def main():
         only_events=[DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent],only_tables=ONLY_Tables)
 
     for binlogevent in stream:       
-	prefix = "%s-%s-" % (binlogevent.schema, binlogevent.table)
+        prefix = "%s-%s-" % (binlogevent.schema, binlogevent.table)
         for row in binlogevent.rows:
             if isinstance(binlogevent, DeleteRowsEvent):
                 vals = row["values"]
-		print "[Delete] SQLBinLog Row :" + str(row)
+                print("[Delete] SQLBinLog Row :" + str(row))
                 updateRediSCache(False,prefix,vals.keys(),vals) 
             elif isinstance(binlogevent, UpdateRowsEvent):
-		before_values = row["before_values"]
-		after_values = row["after_values"]
-		print "[Update] SQLBinLog Row :" + str(row)	
-		updateRediSCache(True,prefix,diff(before_values,after_values).keys(),after_values)
+                before_values = row["before_values"]
+                after_values = row["after_values"]
+                print("[Update] SQLBinLog Row :" + str(row))
+                updateRediSCache(True,prefix,diff(before_values,after_values).keys(),after_values)
             elif isinstance(binlogevent, WriteRowsEvent):
                 vals = row["values"]
-		print "[Insert] SQLBinLog Row :" + str(row)		
-		updateRediSCache(True,prefix,vals.keys(),vals)            
+                print("[Insert] SQLBinLog Row :" + str(row))
+                updateRediSCache(True,prefix,vals.keys(),vals)            
 
     stream.close()
 
